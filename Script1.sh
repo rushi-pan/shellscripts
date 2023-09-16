3fa1a6b4cbd08d5b7383ecca7cd5933522e6050e
Admin <admin@Admins-MacBook-Pro.local>
2023-09-16 12:20:22 +0530

    this is my first commit for batch-20

--- Script1.sh
+++ Script1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Hello batch-19, We are learning Develops"
-echo "husband: No, Lets travel matheran next week"
-echo "Lets travel lonavala this weekend"
+echo "Hello, Batch- 20"
